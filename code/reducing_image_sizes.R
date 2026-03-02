692498c8fa5a755d87c06171c4e91089cdf8b83a
Brendan O'Dowd <brendanjodowd@gmail.com>
2026-03-02 13:34:39 +0000

    editing thumbnails

--- code/reducing_image_sizes.R
+++ code/reducing_image_sizes.R
@@ -7,3 +7,14 @@ for(i in 1:13){
   img_small <- image_resize(img , '600x600')
   image_write(img_small , paste0('resources/thumbnails2/wp' , i ,'.png' ))
 }
+
+
+for(i in 1:13){
+  print(i)
+  img <- image_read(paste0('resources/thumbnails/wp' , i ,'.png' ))
+  img_small <- image_trim(img)
+  image_write(img_small , paste0('resources/thumbnails2/wp' , i ,'.png' ))
+}
+
+
+
