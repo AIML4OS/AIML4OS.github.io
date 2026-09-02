6227f415a8c6f0c214dce4eb0c662435d8dfce60
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-09-02 13:15:49 +0000

    Built site for gh-pages

--- collapsible.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  document.querySelectorAll(".media-collapsible").forEach(function(button){
-    button.addEventListener("click", function(){
-      this.classList.toggle("active");
-    });
-  });
-});
