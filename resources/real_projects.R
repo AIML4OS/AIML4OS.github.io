bdc4801d6ad94f870f45ba38d55655c96c770e95
Brendan O'Dowd <brendanjodowd@gmail.com>
2026-09-10 14:23:42 +0000

    adding two italian projects

--- resources/real_projects.R
+++ resources/real_projects.R
@@ -1,33 +1,51 @@
-real_projects <- bind_rows(tibble(sovereignt = 'Ireland', 
-                        category = 'Classification',
-                        country_id = 1,
-                        proj_title = 'Text Classification for Mortality Statistics',
-                        proj_label = 'In Ireland, the production of statistics on mortality requires the manually classification 
-                        of many thousands of death certificates. The CSO Data Science divison, working in collaboration with 
-                        the Vital Statistics division, have developed a classification assistant which provides manual coders
-                        with suggested classifications for text from death certificates. The application uses a random forest
-                        model which has been trained on three years of data.'), 
-                        tibble(sovereignt = 'Ireland', 
+real_projects <- bind_rows(
+                        tibble(country = 'Ireland', 
+                                category = 'Classification',
+                                proj_title = 'Text Classification for Mortality Statistics',
+                                proj_label = 'In Ireland, the production of statistics on mortality requires the manually classification 
+                                of many thousands of death certificates. The CSO Data Science divison, working in collaboration with 
+                                the Vital Statistics division, have developed a classification assistant which provides manual coders
+                                with suggested classifications for text from death certificates. The application uses a random forest
+                                model which has been trained on three years of data.'), 
+                        tibble(country = 'Ireland', 
                                category = 'Earth Observation',
-                               country_id = 2,
                                proj_title = 'Peatland Mapping',
                                proj_label = 'A machine learning model based on Sentinel 2 data (CDSE) has been developed to identify
                                peatland regions of Ireland. This is used in the production of ecosystem extent accounts, which are
                                required under EU Regulations every three years.'),
-                        tibble(sovereignt = 'Ireland', 
+                        tibble(country = 'Ireland', 
                                category = 'LLM',
-                               country_id = 3,
                                proj_title = 'SAS-R Transcompiler',
                                proj_label = 'A code translation tool developed to help users convert code between statistical programming 
                                languages through a simple web-based interface. Built using R and Shiny, with large language model (LLM) 
                                functionality accessed through the OpenAI API, the tool produces translated code together with explanatory notes.
                                It was featured in a <a href="https://unece.org/sites/default/files/2023-12/HLGMOS%20LLM%20Paper_Preprint_1.pdf" target="_blank">HLG-MOS white paper</a> on the 
                                use of LLMs in official statistics.'),
-                        tibble(sovereignt = 'Ireland', 
+                        tibble(country = 'Ireland', 
                                category = 'Classification',
-                               country_id = 4,
                                proj_title = 'NACE Classification Assistant',
                                proj_label = 'This application provides suggested NACE codes for input text that describes activities of enterprises.
                                It uses a machine learning model which has been trained on a collection of over 1 million descriptions of principal
                                activity. It also uses string-matching to terms related to different NACE codes, which is particularly useful for 
-                               rarer NACE categories which do not appear frequently in the corpus of training data.'))
+                               rarer NACE categories which do not appear frequently in the corpus of training data.'),
+                        
+                        tibble(country = 'Italy', 
+                               category = 'Imputation',
+                               proj_title = 'Advanced artificial intelligence techniques to improve survey data quality: A focus on transformers and generative adversarial networks (GANs)',
+                               proj_label = 'Machine learning and deep learning techniques, including transformers and GANs, are used to improve 
+                               missing data imputation in social surveys, enhancing data quality in health, environment, and social inequality 
+                               domains. Applied to ISTAT’s large Aspects of Daily Life survey, these AI-driven methods outperform traditional 
+                               approaches by better preserving data variance, offering more reliable tools for policy-making and future research.
+                               <a href="https://unece.org/sites/default/files/2025-05/GenAI2025_S2_Italy_Cafieri_D.pdf" target="_blank">Link</a>'),
+                        tibble(country = 'Italy', 
+                               category = 'Dissemination',
+                               proj_title = 'AI Solutions in Istat Contact Centre for Knowledge Seekers, Data Users and Survey Respondents',
+                               proj_label = 'Istat uses AI to improve its Contact Centre by enabling users to find answers independently, 
+                               reducing support requests and enhancing satisfaction. A generative AI chatbot powered by Salesforce Einstein 
+                               provides personalized guidance, while machine learning classifies user issues to link relevant knowledge base 
+                               content. This approach streamlines operator work, speeds up responses, and lowers operational costs, improving 
+                               overall user experience and internal efficiency.
+                               <a href="https://cros.ec.europa.eu/system/files/2025-02/Book%20of%20Abstracts%20-%20NTTS%202025%20-%2027.02.25.pdf" target="_blank">Link</a>')
+                        
+                        
+                        )
